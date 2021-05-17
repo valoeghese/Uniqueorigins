15771f13b3bb802a838f651eb31b6c115e62fd0a
valoeghese <valoeghesemods@gmail.com>
2021-05-17 13:01:37 +1300

    start uniqueorigins

---
+++ src/main/java/valoeghese/uniqueorigins/Uniqueorigins.java
@@ -0,0 +1,34 @@
+/*
+ * Uniqueorigins
+ * Copyright (C) 2020 Valoeghese
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package valoeghese.uniqueorigins;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Uniqueorigins implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Uniqueorigins");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Making sure your origins will be more... unique~");
+	}
+}
